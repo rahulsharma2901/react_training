744dc15c693eab9785d65c12582b83139d452113
rahulsharma2901 <rahul.s@applocum.org>
2024-03-12 14:12:52 +0530

    Examples of React Router Dom

--- Chapter-5/react_router/src/App.js
+++ Chapter-5/react_router/src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NavBar from './pages/NavBar';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
+  const navigate = useNavigate();
+
+  const navigateToWhere = () => {
+    let name = "Sneha";
+    if( name == 'Rahul'){
+      navigate('/about');
+    } else {
+      navigate('/contact');
+    }
+  }
+  
   return (
     <>
     <NavBar/>
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<PageNotFound />}/>
       </Routes>
+      <button onClick={() => navigateToWhere()}>Click Me</button> <br />
+      <button onClick={() => navigate(-1)}>Go back</button>
     </>
   );
 }
