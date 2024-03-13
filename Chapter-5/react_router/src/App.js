6bc32ce9bc697535fe208dc2d9fba26016b3764b
rahulsharma2901 <rahul.s@applocum.org>
2024-03-13 11:00:18 +0530

    Examples of React Router

--- Chapter-5/react_router/src/App.js
+++ Chapter-5/react_router/src/App.js
@@ -1,52 +1,24 @@
 import logo from './logo.svg';
-import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import NavBar from './pages/NavBar';
-import PageNotFound from './pages/PageNotFound';
-import Products from './pages/Products';
-import Shirts from './pages/Shirts';
-import Accessories from './pages/Accessories';
-import Shoes from './pages/Shoes';
-import Jeans from './pages/Jeans';
-import Users from './pages/Users';
-import UserDetails from './pages/UserDetails';
 
 function App() {
-  const navigate = useNavigate();
-
-  const navigateToWhere = () => {
-    let name = "Sneha";
-    if (name == 'Rahul') {
-      navigate('/about');
-    } else {
-      navigate('/contact');
-    }
-  }
-
   return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} >
-          <Route index element= {<Shirts />} />
-          <Route path='shirts' element={<Shirts />} />
-          <Route path='jeans' element={<Jeans />} />
-          <Route path='shoes' element={<Shoes />} />
-          <Route path='accessories' element={<Accessories />} />
-        </Route>
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/users/:id' element={<UserDetails />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-      <button onClick={() => navigateToWhere()}>Click Me</button> <br />
-      <button onClick={() => navigate(-1)}>Go back</button>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
